312e43a6b1d6a76c7c287b710f512cd346b97ece
Poisonlocket <loribono@gmx.ch>
2023-11-16 08:56:37 +0100

    Added new background improved / darkened color scheme

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,8 @@ module.exports = {
   theme: {
     extend: {
       backgroundImage: {
-        parallax: 'url("/img/bg.png")',
+        parallax:
+          'url("/img/web_zurich_paradeplatz_tram_zt_8121_1600x900.jpg")',
       },
       spacing: {
         128: "32rem",
