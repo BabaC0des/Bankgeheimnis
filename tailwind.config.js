e5ba4568d7db8592bca5152b33a53bb0e78d2b5d
Poisonlocket <loribono@gmx.ch>
2023-11-16 20:09:14 +0100

    Added Responsive Footer, fixed some other things

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,7 @@ module.exports = {
     extend: {
       backgroundImage: {
         parallax:
-          'url("/img/web_zurich_paradeplatz_tram_zt_8121_1600x900.jpg")',
+          'url("/img/web_zurich_paradeplatz_tram_zt_8121_1600x900 - Kopie.jpg")',
       },
       spacing: {
         128: "32rem",
